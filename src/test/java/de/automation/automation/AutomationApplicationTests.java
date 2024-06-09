4a12fd2ca945b1d166e49b7a2050af0ae7ff43a1
mzdih <mehdi.zdih@qytera.de>
2024-06-09 22:38:17 +0200

    use chrome webdriver

--- src/test/java/de/automation/automation/AutomationApplicationTests.java
+++ src/test/java/de/automation/automation/AutomationApplicationTests.java
@@ -2,43 +2,27 @@ package de.automation.automation;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 @SpringBootTest
 class AutomationApplicationTests extends AbstractTestNGSpringContextTests {
 
     @Test
-    void contextLoads() throws MalformedURLException {
-        chromeDriver().get("https://www.qytera.de/");
+    void contextLoads() {
+        WebDriver driver = chromeDriver();
+        driver.get("https://www.qytera.de/");
     }
 
-    public RemoteWebDriver chromeDriver() throws MalformedURLException {
+    public static WebDriver chromeDriver() {
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.setBrowserVersion("116.0.5845.111");
-        options.addArguments("--headless");  // Optional: Run Chrome in headless mode
-        options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
-        options.addArguments("--safebrowsing-disable-auto-update ");
-        options.addArguments("--disable-background-networking");
-        options.addArguments("--no-proxy-server");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--dns-prefetch-disable");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--force-device-scale-factor=1");
-        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
-
-        // URL of the remote Selenium Grid hub
-        //String remoteWebDriverUrl = System.getProperty("remote.webdriver.url", "http://localhost:4444/wd/hub");
-        URL hubUrl = new URL("http://localhost:4444/wd/hub");
-        return new RemoteWebDriver(hubUrl, options);
+        options.addArguments("--headless");  // Run Chrome in headless mode
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--disable-dev-shm-usage");
+        return new ChromeDriver(options);
     }
 }
