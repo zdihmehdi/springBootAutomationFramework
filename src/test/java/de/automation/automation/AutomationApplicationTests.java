e8ce33d99b46bab9ff1675365841b4f44f674577
mzdih <mehdi.zdih@qytera.de>
2024-06-09 22:44:01 +0200

    use chrome webdriver

--- src/test/java/de/automation/automation/AutomationApplicationTests.java
+++ src/test/java/de/automation/automation/AutomationApplicationTests.java
@@ -3,8 +3,8 @@ package de.automation.automation;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
@@ -13,16 +13,16 @@ class AutomationApplicationTests extends AbstractTestNGSpringContextTests {
 
     @Test
     void contextLoads() {
-        WebDriver driver = chromeDriver();
+        WebDriver driver = edgeDriver();
         driver.get("https://www.qytera.de/");
     }
 
-    public static WebDriver chromeDriver() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
+    public static WebDriver edgeDriver() {
+        EdgeOptions options = new EdgeOptions();
         options.addArguments("--headless");  // Run Chrome in headless mode
         options.addArguments("--no-sandbox"); // Bypass OS security model
         options.addArguments("--disable-dev-shm-usage");
-        return new ChromeDriver(options);
+        WebDriverManager.edgedriver().setup();
+        return new EdgeDriver(options);
     }
 }
