88a5c7a365ed28ab284aebf6ded87616111c9a1d
mzdih <mehdi.zdih@qytera.de>
2024-06-10 00:44:53 +0200

    use chrome webdriver

--- src/test/java/de/automation/automation/AutomationApplicationTests.java
+++ src/test/java/de/automation/automation/AutomationApplicationTests.java
@@ -3,26 +3,33 @@ package de.automation.automation;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @SpringBootTest
 class AutomationApplicationTests extends AbstractTestNGSpringContextTests {
 
     @Test
-    void contextLoads() {
-        WebDriver driver = edgeDriver();
+    void contextLoads() throws MalformedURLException {
+        WebDriver driver = chromeDriver();
         driver.get("https://www.qytera.de/");
     }
 
-    public static WebDriver edgeDriver() {
-        EdgeOptions options = new EdgeOptions();
-        options.addArguments("--headless");  // Run Chrome in headless mode
-        options.addArguments("--no-sandbox"); // Bypass OS security model
+    public static WebDriver chromeDriver() throws MalformedURLException {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        WebDriverManager.edgedriver().setup();
-        return new EdgeDriver(options);
+
+        // URL of the Selenium hub
+        String seleniumHubUrl = "http://localhost:4444/wd/hub";
+        return new RemoteWebDriver(new URL(seleniumHubUrl), options);
     }
 }
