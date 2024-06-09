882264b335404067d089534ff9c358c02d996284
mzdih <mehdi.zdih@qytera.de>
2024-06-10 01:11:59 +0200

    use chrome webdriver

--- src/test/java/de/automation/automation/AutomationApplicationTests.java
+++ src/test/java/de/automation/automation/AutomationApplicationTests.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.http.ClientConfig;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
 @SpringBootTest
 class AutomationApplicationTests extends AbstractTestNGSpringContextTests {
@@ -24,10 +26,6 @@ class AutomationApplicationTests extends AbstractTestNGSpringContextTests {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-
         // URL of the Selenium hub
         String seleniumHubUrl = "http://localhost:4444/wd/hub";
         return new RemoteWebDriver(new URL(seleniumHubUrl), options);
