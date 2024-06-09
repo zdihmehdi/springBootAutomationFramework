099f7e5840e9a1a11295b7b2386281ef533a0a06
mzdih <mehdi.zdih@qytera.de>
2024-06-09 21:52:58 +0200

    use chrome webdriver

--- src/test/java/de/automation/automation/AutomationApplicationTests.java
+++ src/test/java/de/automation/automation/AutomationApplicationTests.java
@@ -5,23 +5,30 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @SpringBootTest
 class AutomationApplicationTests extends AbstractTestNGSpringContextTests {
 
     @Test
-    void contextLoads() {
-        WebDriver driver = chromeDriver();
-        driver.get("https://www.qytera.de/");
+    void contextLoads() throws MalformedURLException {
+        chromeDriver().get("https://www.qytera.de/");
     }
 
-    public static WebDriver chromeDriver() {
-        WebDriverManager.chromedriver().setup();
+    public RemoteWebDriver chromeDriver() throws MalformedURLException {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--start-maximized");
-        return new ChromeDriver(options);
+        options.addArguments("--headless");  // Optional: Run Chrome in headless mode
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
+
+        // URL of the remote Selenium Grid hub
+        //String remoteWebDriverUrl = System.getProperty("remote.webdriver.url", "http://localhost:4444/wd/hub");
+        URL hubUrl = new URL("http://localhost:4444/wd/hub");
+        return new RemoteWebDriver(hubUrl, options);
     }
 }
