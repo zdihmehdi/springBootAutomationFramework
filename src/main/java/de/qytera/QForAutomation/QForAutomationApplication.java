1404df99c1b124d9ba824cee0ce23440c660301c
mzdih <mehdi.zdih@qytera.de>
2024-06-14 11:42:45 +0200

    empty spring project

--- src/main/java/de/qytera/QForAutomation/QForAutomationApplication.java
+++ src/main/java/de/qytera/QForAutomation/QForAutomationApplication.java
@@ -1,13 +1,13 @@
-package de.automation.automation;
+package de.qytera.QForAutomation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AutomationApplication {
+public class QForAutomationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AutomationApplication.class, args);
+		SpringApplication.run(QForAutomationApplication.class, args);
 	}
 
 }
---
+++ src/test/java/de/qytera/QForAutomation/QForAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package de.qytera.QForAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QForAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
