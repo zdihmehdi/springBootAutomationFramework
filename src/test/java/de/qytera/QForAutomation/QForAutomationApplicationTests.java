f023b08856cdab60917ec27cef3aafe763121a4a
mzdih <mehdi.zdih@qytera.de>
2024-06-14 14:57:31 +0200

    implement first TA test under spring boot project

--- src/test/java/de/qytera/QForAutomation/QForAutomationApplicationTests.java
+++ src/test/java/de/qytera/QForAutomation/QForAutomationApplicationTests.java
@@ -1,10 +1,13 @@
 package de.qytera.QForAutomation;
 
+import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import com.codeborne.selenide.Configuration;
 
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
@@ -13,22 +16,53 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 @SpringBootTest
 class QForAutomationApplicationTests {
 
-	@BeforeSuite
-	public void setUp() {
-		// Set up WebDriverManager to automatically download the WebDriver binary
-		WebDriverManager.chromedriver().setup();
+    //@BeforeMethod
+    public void setUp() {
+        // Set up WebDriverManager to automatically download the WebDriver binary
+        WebDriverManager.chromedriver().setup();
 
-		// Set up the browser and other configurations
-		Configuration.browser = "chrome";
-		getWebDriver().manage().window().maximize();
-	}
+        // Configure Selenide to use the local WebDriver
+        Configuration.browser = "chrome";
 
-	@Test
-	void contextLoads() {
-		open("https://www.qytera.de/");
+        // Set Chrome options to run in headless mode
+        ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--headless");
+        options.addArguments("--disable-gpu"); // applicable to Windows OS
+        options.addArguments("--window-size=1920,1080");
 
-		// Click on the "Kontakt" link
-		$x("//a[contains(text(), 'Kontakt')]").click();
-	}
+        // Apply the Chrome options
+        Configuration.browserCapabilities = options;
+    }
+
+    //@BeforeSuite
+    public void setUpRemoteDriver() {
+        // Configure Selenide to use the remote WebDriver
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.browser = "remote-chrome";
+
+        // Set Chrome options to run in headless mode
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu"); // applicable to Windows OS
+        options.addArguments("--window-size=1920,1080");
+
+        // Set desired capabilities
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("remote-chrome");
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        Configuration.browserCapabilities = capabilities;
+
+        // Ensure the remote driver is used
+        //Configuration.driverManagerEnabled = false;
+    }
+
+    @Test
+    void contextLoads() {
+        System.out.println("TEST BEGIN");
+        open("https://www.qytera.de/");
+        // Click on the "Kontakt" link
+        $x("//a[contains(text(), 'Kontakt')]").click();
+        System.out.println("TEST FINISHED");
+    }
 
 }
